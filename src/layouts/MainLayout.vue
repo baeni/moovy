871c78b451ac9f20435a7971da59e60eac3a3459
baeni <baeni.saa@gmail.com>
2021-08-14 19:40:37 +0200

    Initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <!-- TODO: Navbar -->
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <!-- TODO: Footer -->
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout'
+}
+</script>
