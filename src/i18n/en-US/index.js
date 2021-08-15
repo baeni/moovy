7943443d1ec2670dd3639f50a5692206255f4200
baeni <baeni.saa@gmail.com>
2021-08-15 16:03:23 +0200

    Update en-US

--- src/i18n/en-US/index.js
+++ src/i18n/en-US/index.js
@@ -2,5 +2,15 @@
 // so you can safely delete all default props below
 
 export default {
-
+  general: {
+    formSearch: 'Search for any movie'
+  },
+  indexPage: {
+    home: {
+      name: 'Home',
+      heading: 'Learn everything about your favourite moovy',
+      subHeading: 'Pronounced as /mu:vɪ/ or movie'
+    }
+  },
+  moviePage: {}
 }
