d61aa701c99cfb5c0727963353b688daf13b9da4
baeni <baeni.saa@gmail.com>
2021-08-15 16:03:44 +0200

    Create Navbar.vue

---
+++ src/components/general/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-header>
+    <q-toolbar class="container">
+      <q-btn class="text-h3" label="moovy" flat @click="$router.push('/')" />
+
+      <q-space />
+
+      <q-btn
+        class="text-bold"
+        :label="$t('indexPage.home.name')"
+        flat
+        @click="$router.push('/')"
+      />
+
+      <search-form class="q-pl-md" />
+    </q-toolbar>
+  </q-header>
+</template>
+
+<script>
+import SearchForm from "components/general/SearchForm";
+
+export default {
+  name: 'Navbar',
+  components: {
+    SearchForm
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+  background: transparentize($dark, .65);
+  backdrop-filter: blur(15px);
+}
+</style>
