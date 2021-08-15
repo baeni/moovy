b841e5a073a2af3f2067fea967fd029482d023cb
baeni <baeni.saa@gmail.com>
2021-08-15 16:03:47 +0200

    Create SearchForm.vue

---
+++ src/components/general/SearchForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-form class="row" @submit="searchMovies(searchQuery)">
+    <q-input class="full-width" v-model="searchQuery" :placeholder="$t('general.formSearch')" dark dense standout>
+      <template v-slot:append>
+        <q-icon name="search" />
+      </template>
+    </q-input>
+  </q-form>
+</template>
+
+<script>
+export default {
+  name: 'SearchForm',
+  data() {
+    return {
+      searchQuery: ""
+    }
+  },
+  methods: {
+    searchMovies(searchQuery) {
+      this.$store.dispatch('SearchModule/searchMovies', searchQuery);
+    }
+  }
+}
+</script>
