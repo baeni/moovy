3431e9fd3f04058a39fb3921ca87dc2409fffda6
baeni <baeni.saa@gmail.com>
2021-08-16 17:43:17 +0200

    Make clearable; Add `dense`-property to make hint not visible

--- src/components/general/SearchForm.vue
+++ src/components/general/SearchForm.vue
@@ -1,8 +1,22 @@
 <template>
   <q-form class="row" @submit="searchMovies(searchQuery)">
-    <q-input class="full-width" v-model="searchQuery" :placeholder="$t('general.formSearch')" dark dense standout>
+    <q-input
+      class="full-width"
+      v-model="searchQuery"
+      :model-value="searchQuery"
+      :placeholder="$t('general.formSearch')"
+      :bottom-slots="!dense"
+      clear-icon="close"
+      clearable
+      dark
+      dense
+      standout
+    >
       <template v-slot:append>
         <q-icon name="search" />
+      </template>
+      <template v-slot:hint v-if="!dense">
+        {{ $t('general.formSearchHint') }}
       </template>
     </q-input>
   </q-form>
@@ -11,6 +25,12 @@
 <script>
 export default {
   name: 'SearchForm',
+  props: {
+    dense: {
+      type: Boolean,
+      default: false
+    }
+  },
   data() {
     return {
       searchQuery: ""
