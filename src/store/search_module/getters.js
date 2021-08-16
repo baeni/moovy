e9593748cc3b7e7316f190dd84c2a567a3662f47
baeni <baeni.saa@gmail.com>
2021-08-16 17:45:55 +0200

    Add several getters

--- src/store/search_module/getters.js
+++ src/store/search_module/getters.js
@@ -1,3 +1,21 @@
-export function getDisplayedMovies (state) {
+export function getNowPlayingMovies(state) {
+  return state.nowPlayingMovies;
+}
+
+export function getTrendyMoviesDay(state) {
+  return state.trendyMoviesDay
+}
+
+export function getTrendyMoviesWeek(state) {
+  return state.trendyMoviesWeek;
+}
+
+export function getPopularActors(state) {
+  return state.popularActors;
+}
+
+
+
+export function getDisplayedMovies(state) {
   return state.displayedMovies;
 }
