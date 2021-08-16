84f43bcda20b5396e6f885267caa28d61d5049c2
baeni <baeni.saa@gmail.com>
2021-08-16 17:45:48 +0200

    Add several mutations

--- src/store/search_module/mutations.js
+++ src/store/search_module/mutations.js
@@ -1,3 +1,21 @@
-export function setDisplayedMovies (state, displayedMovies) {
-  state.displayedMovies = displayedMovies;
+export function setNowPlayingMovies(state, movies) {
+  state.nowPlayingMovies = movies;
+}
+
+export function setTrendyMoviesDay(state, movies) {
+  state.trendyMoviesDay = movies;
+}
+
+export function setTrendyMoviesWeek(state, movies) {
+  state.trendyMoviesWeek = movies;
+}
+
+export function setPopularActors(state, actors) {
+  state.popularActors = actors;
+}
+
+
+
+export function setDisplayedMovies(state, movies) {
+  state.displayedMovies = movies;
 }
