590658bc30a78c8fe213b072c31076fd3713870c
baeni <baeni.saa@gmail.com>
2021-08-16 17:46:54 +0200

    Implement new components; Minor adjustments

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,42 @@
 <template>
-  <q-page class="absolute-top">
-    <section id="landing" class="flex flex-center">
+  <q-page>
+    <section>
       <div class="container">
-        <div class="text-h1 text-gradient text-center">{{ $t('indexPage.home.heading') }}</div>
-        <div class="text-h2 text-light text-center q-pb-xl">{{ $t('indexPage.home.subHeading') }}</div>
+        <div class="text-h1 text-center text-gradient">{{ $t('indexPage.heading') }}</div>
+        <div class="text-h2 text-center text-light">{{ $t('indexPage.subHeading') }}</div>
+        <search-form class="q-pt-lg" />
+      </div>
+    </section>
 
-        <search-form />
-        <search-suggestion />
+    <q-separator dark inset />
+
+    <section>
+      <div class="container">
+        <movies-area :title="$t('indexPage.nowPlayingMovies')" :movies="$store.getters['SearchModule/getNowPlayingMovies']" />
+      </div>
+    </section>
+
+    <q-separator dark inset />
+
+    <section>
+      <div class="container">
+        <movies-area :title="$t('indexPage.trendyMoviesDay')" :movies="$store.getters['SearchModule/getTrendyMoviesDay']" />
+      </div>
+    </section>
+
+    <q-separator dark inset />
+
+    <section>
+      <div class="container">
+        <movies-area :title="$t('indexPage.trendyMoviesWeek')" :movies="$store.getters['SearchModule/getTrendyMoviesWeek']" />
+      </div>
+    </section>
+
+    <q-separator dark inset />
+
+    <section>
+      <div class="container">
+        <actors-area :title="$t('indexPage.popularActors')" :actors="$store.getters['SearchModule/getPopularActors']" />
       </div>
     </section>
   </q-page>
@@ -14,26 +44,21 @@
 
 <script>
 import SearchForm from "components/general/SearchForm";
-import SearchSuggestion from "components/index_page/SearchSuggestion";
+import MoviesArea from "components/index_page/MoviesArea";
+import ActorsArea from "components/index_page/ActorsArea";
 
 export default {
   name: 'IndexPage',
   components: {
     SearchForm,
-    SearchSuggestion
+    MoviesArea,
+    ActorsArea
   },
   mounted() {
-    this.$store.dispatch('SearchModule/fetchTrendy')
+    this.$store.dispatch('SearchModule/fetchNowPlayingMovies');
+    this.$store.dispatch('SearchModule/fetchTrendyMovies', "Day");
+    this.$store.dispatch('SearchModule/fetchTrendyMovies', "Week");
+    this.$store.dispatch('SearchModule/fetchPopularActors');
   }
 }
 </script>
-
-<style lang="scss" scoped>
-#landing {
-  height: 100vh;
-  background: url("../assets/cinema.jpg");
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
-}
-</style>
