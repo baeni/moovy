0ec0223e587ae03a53c1e60c6751e4a91806ac4e
baeni <baeni.saa@gmail.com>
2021-08-16 17:46:12 +0200

    Add several actions

--- src/store/search_module/actions.js
+++ src/store/search_module/actions.js
@@ -1,21 +1,30 @@
 import axios from "axios";
 
-export function fetchTrendy(context) {
-  axios.get('https://api.themoviedb.org/3/trending/movie/day?api_key=f69ebea429f8ce30593ffaeed4c3e712')
+export function fetchNowPlayingMovies(context) {
+  axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=f69ebea429f8ce30593ffaeed4c3e712`)
     .then(response => {
-      context.commit('setDisplayedMovies', response.data.results);
+      context.commit(`setNowPlayingMovies`, response.data.results);
     })
     .catch(err => Promise.reject(err));
 }
 
-export function searchGenre(context, genreId) {
-  axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=f69ebea429f8ce30593ffaeed4c3e712&with_genres=${genreId}`)
+export function fetchTrendyMovies(context, timeWindow) {
+  axios.get(`https://api.themoviedb.org/3/trending/movie/${timeWindow}?api_key=f69ebea429f8ce30593ffaeed4c3e712`)
     .then(response => {
-      context.commit('setDisplayedMovies', response.data.results);
-      this.$router.push('/movie');
+      context.commit(`setTrendyMovies${timeWindow}`, response.data.results);
     })
     .catch(err => Promise.reject(err));
 }
+
+export function fetchPopularActors(context) {
+  axios.get(`https://api.themoviedb.org/3/person/popular?api_key=f69ebea429f8ce30593ffaeed4c3e712`)
+    .then(response => {
+      context.commit(`setPopularActors`, response.data.results);
+    })
+    .catch(err => Promise.reject(err));
+}
+
+
 
 export function searchMovies(context, searchQuery) {
   axios.get(`https://api.themoviedb.org/3/search/movie?api_key=f69ebea429f8ce30593ffaeed4c3e712&query=${searchQuery}`)
