855d65852faee80640968f3018ccb1e1345b725c
baeni <baeni.saa@gmail.com>
2021-08-19 17:38:23 +0200

    Create TrailerListItem.vue

---
+++ src/components/TrailerListItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="col-6">
+    <q-card
+      class="full-height"
+      bordered
+      dark
+    >
+      <div class="overflow-hidden">
+        <q-video
+          :src="'https://www.youtube-nocookie.com/embed/'+trailer.key+'?start=0'"
+          :ratio="1.85"
+        />
+      </div>
+
+      <q-card-section>
+        <div class="text-subtitle2 text-light">{{ trailer.published_at }}</div>
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TrailerListItem',
+  props: {
+    trailer: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
