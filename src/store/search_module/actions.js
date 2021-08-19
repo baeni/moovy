e965a1f34c8a76d8027258cea9a33580b87dd432
baeni <baeni.saa@gmail.com>
2021-08-19 17:47:11 +0200

    Add `return` keyword for axios functions; Adjustments to functions; Change language query to get from i18n

--- src/store/search_module/actions.js
+++ src/store/search_module/actions.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import { useI18n } from 'vue-i18n';
 
 export function fetchNowPlayingMovies(context) {
-  axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=f69ebea429f8ce30593ffaeed4c3e712`)
+  return axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=f69ebea429f8ce30593ffaeed4c3e712&language=${useI18n().locale}`)
     .then(response => {
       context.commit(`setNowPlayingMovies`, response.data.results);
     })
@@ -9,7 +10,7 @@ export function fetchNowPlayingMovies(context) {
 }
 
 export function fetchTrendyMovies(context, timeWindow) {
-  axios.get(`https://api.themoviedb.org/3/trending/movie/${timeWindow}?api_key=f69ebea429f8ce30593ffaeed4c3e712`)
+  return axios.get(`https://api.themoviedb.org/3/trending/movie/${timeWindow}?api_key=f69ebea429f8ce30593ffaeed4c3e712&language=${useI18n().locale}`)
     .then(response => {
       context.commit(`setTrendyMovies${timeWindow}`, response.data.results);
     })
@@ -17,20 +18,25 @@ export function fetchTrendyMovies(context, timeWindow) {
 }
 
 export function fetchPopularActors(context) {
-  axios.get(`https://api.themoviedb.org/3/person/popular?api_key=f69ebea429f8ce30593ffaeed4c3e712`)
+  return axios.get(`https://api.themoviedb.org/3/person/popular?api_key=f69ebea429f8ce30593ffaeed4c3e712&language=${useI18n().locale}`)
     .then(response => {
       context.commit(`setPopularActors`, response.data.results);
     })
     .catch(err => Promise.reject(err));
 }
 
-
-
-export function searchMovies(context, searchQuery) {
-  axios.get(`https://api.themoviedb.org/3/search/movie?api_key=f69ebea429f8ce30593ffaeed4c3e712&query=${searchQuery}`)
+export function searchMovie(context, searchQuery) {
+  return axios.get(`https://api.themoviedb.org/3/search/movie?api_key=f69ebea429f8ce30593ffaeed4c3e712&query=${searchQuery}&language=${useI18n().locale}`)
     .then(response => {
-      context.commit('setDisplayedMovies', response.data.results);
-      this.$router.push('/movie');
+      return response.data.results;
+    })
+    .catch(err => Promise.reject(err));
+}
+
+export function getMovieDetails(context, movieId) {
+  return axios.get(`https://api.themoviedb.org/3/movie/${movieId}?append_to_response=videos&api_key=f69ebea429f8ce30593ffaeed4c3e712&language=${useI18n().locale}`)
+    .then(response => {
+      return response.data;
     })
     .catch(err => Promise.reject(err));
 }
