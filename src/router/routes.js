425a2a87cb31c32c85ebc0b2914aa99554afbd98
baeni <baeni.saa@gmail.com>
2021-08-19 17:43:33 +0200

    Change path's for routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,19 +1,15 @@
-
 const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: 'movie', component: () => import('pages/MoviePage.vue') }
-    ]
-  },
 
-  // Always leave this as last one,
-  // but you can also remove it
-  {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/Error404Page.vue')
+      { path: 'search/movie/:searchQuery', component: () => import('pages/SearchPage.vue') },
+      { path: 'details/movie/:id', component: () => import('pages/DetailsPage.vue') },
+
+      { path: ':catchAll(.*)*', component: () => import('pages/Error404Page.vue') }
+    ]
   }
 ]
 
