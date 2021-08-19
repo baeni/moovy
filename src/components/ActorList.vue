ad898921678e9f1ec033941d4ece801c6d8220b9
baeni <baeni.saa@gmail.com>
2021-08-19 17:35:23 +0200

    Create ActorList.vue

---
+++ src/components/ActorList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="text-h2 text-lightest q-pb-md">{{ title }}</div>
+  <div class="row q-col-gutter-md">
+    <actor-list-item v-for="(actor, i) in actors" :key="i" :actor="actor" />
+  </div>
+</template>
+
+<script>
+import ActorListItem from "components/ActorListItem";
+
+export default {
+  name: 'ActorList',
+  components: {
+    ActorListItem
+  },
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    actors: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
