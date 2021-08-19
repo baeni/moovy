0a8f2781fe653e7caaf3df44feeef7d55c690ba5
baeni <baeni.saa@gmail.com>
2021-08-19 17:41:40 +0200

    Create index.js

---
+++ src/i18n/de/index.js
@@ -0,0 +1,22 @@
+export default {
+  general: {
+    formSearch: 'Suche nach einem Film',
+    formSearchHint: 'moovy basiert auf der TMDb und bezieht alle Informationen von dessen API'
+  },
+  indexPage: {
+    name: 'Home',
+    heading: 'Finde den Film mit moovy',
+    subHeading: 'Gesprochen wie /mu:vɪ/ oder movie',
+    nowPlayingMovies: 'Jetzt im Kino',
+    trendyMoviesDay: 'Heute angesagt',
+    trendyMoviesWeek: 'Diese Woche angesagt',
+    popularActors: 'Beliebte Schauspieler'
+  },
+  searchPage: {
+    results: 'Ergebnisse für "{searchQuery}"'
+  },
+  detailsPage: {
+    homepage: 'Offizielle Seite',
+    trailers: 'Trailer, Teaser & mehr'
+  }
+}
