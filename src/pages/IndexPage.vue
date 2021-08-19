f1148f0e3238f799653b854c40e3d5bd38b8e387
baeni <baeni.saa@gmail.com>
2021-08-19 17:44:10 +0200

    Rename components; Minor changes

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -3,7 +3,7 @@
     <section>
       <div class="container">
         <div class="text-h1 text-center text-gradient">{{ $t('indexPage.heading') }}</div>
-        <div class="text-h2 text-center text-light">{{ $t('indexPage.subHeading') }}</div>
+        <div class="text-h3 text-center text-light">{{ $t('indexPage.subHeading') }}</div>
         <search-form class="q-pt-lg" />
       </div>
     </section>
@@ -12,7 +12,7 @@
 
     <section>
       <div class="container">
-        <movies-area :title="$t('indexPage.nowPlayingMovies')" :movies="$store.getters['SearchModule/getNowPlayingMovies']" />
+        <movie-showcase :title="$t('indexPage.nowPlayingMovies')" :movies="$store.getters['SearchModule/getNowPlayingMovies']" />
       </div>
     </section>
 
@@ -20,7 +20,7 @@
 
     <section>
       <div class="container">
-        <movies-area :title="$t('indexPage.trendyMoviesDay')" :movies="$store.getters['SearchModule/getTrendyMoviesDay']" />
+        <movie-showcase :title="$t('indexPage.trendyMoviesDay')" :movies="$store.getters['SearchModule/getTrendyMoviesDay']" />
       </div>
     </section>
 
@@ -28,7 +28,7 @@
 
     <section>
       <div class="container">
-        <movies-area :title="$t('indexPage.trendyMoviesWeek')" :movies="$store.getters['SearchModule/getTrendyMoviesWeek']" />
+        <movie-showcase :title="$t('indexPage.trendyMoviesWeek')" :movies="$store.getters['SearchModule/getTrendyMoviesWeek']" />
       </div>
     </section>
 
@@ -36,25 +36,25 @@
 
     <section>
       <div class="container">
-        <actors-area :title="$t('indexPage.popularActors')" :actors="$store.getters['SearchModule/getPopularActors']" />
+        <actor-list :title="$t('indexPage.popularActors')" :actors="$store.getters['SearchModule/getPopularActors']" />
       </div>
     </section>
   </q-page>
 </template>
 
 <script>
-import SearchForm from "components/general/SearchForm";
-import MoviesArea from "components/index_page/MoviesArea";
-import ActorsArea from "components/index_page/ActorsArea";
+import SearchForm from "components/SearchForm";
+import MovieShowcase from "components/MovieShowcase";
+import ActorList from "components/ActorList";
 
 export default {
   name: 'IndexPage',
   components: {
     SearchForm,
-    MoviesArea,
-    ActorsArea
+    MovieShowcase,
+    ActorList
   },
-  mounted() {
+  created() {
     this.$store.dispatch('SearchModule/fetchNowPlayingMovies');
     this.$store.dispatch('SearchModule/fetchTrendyMovies', "Day");
     this.$store.dispatch('SearchModule/fetchTrendyMovies', "Week");
