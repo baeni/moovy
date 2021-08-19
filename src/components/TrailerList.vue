3382fa8162c81aae409cd853d4c92fca81ee2735
baeni <baeni.saa@gmail.com>
2021-08-19 17:38:21 +0200

    Create TrailerList.vue

---
+++ src/components/TrailerList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="text-h2 text-lightest q-pb-md">{{ $t('detailsPage.trailers') }}</div>
+  <div class="row q-col-gutter-md">
+    <trailer-item v-for="(trailer, i) in trailers" :key="i" :trailer="trailer" />
+  </div>
+</template>
+
+<script>
+import TrailerItem from "components/TrailerListItem";
+
+export default {
+  name: 'TrailerList',
+  components: {
+    TrailerItem
+  },
+  props: {
+    trailers: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
