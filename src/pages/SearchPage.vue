1a5cdb548cefc3957832cfb1d2f2440b4a6f9d01
baeni <baeni.saa@gmail.com>
2021-08-21 12:09:32 +0200

    Adjust code to work with new discoverysearch

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -19,13 +19,39 @@ export default {
   data() {
     return {
       searchQuery: this.$route.params.searchQuery,
+      discoverQuery: this.$route.params.discoverQuery,
       movies: []
     }
   },
   async mounted() {
-    await this.$store.dispatch('SearchModule/searchMovie', this.searchQuery)
-      .then(response => this. movies = response)
-      .catch(err => Promise.reject(err));
+    if(this.searchQuery) {
+      await this.$store.dispatch('SearchModule/searchMovie', this.searchQuery)
+        .then(response => this.movies = response)
+        .catch(err => Promise.reject(err));
+    } else if(this.discoverQuery) {
+      switch(this.discoverQuery) {
+        case 'now-playing':
+          this.movies = await this.$store.getters["SearchModule/getNowPlayingMovies"];
+          break;
+
+        case 'trending-day':
+          this.movies = await this.$store.getters["SearchModule/getTrendingMoviesDay"];
+          break;
+
+        case 'trending-week':
+          this.movies = await this.$store.getters["SearchModule/getTrendingMoviesWeek"];
+          break;
+
+      }
+    }
+  },
+  created() {
+    if(this.discoverQuery) {
+      this.$store.dispatch('SearchModule/fetchNowPlayingMovies');
+      this.$store.dispatch('SearchModule/fetchTrendingMovies', "Day");
+      this.$store.dispatch('SearchModule/fetchTrendingMovies', "Week");
+      this.$store.dispatch('SearchModule/fetchPopularActors');
+    }
   }
 }
 </script>
