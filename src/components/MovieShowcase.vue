5b11930730495a372289164a23f255e4fef369c3
baeni <baeni.saa@gmail.com>
2021-08-21 12:10:38 +0200

    Make `MovieShowcase` no longer a scroll area; Minor adjustments

--- src/components/MovieShowcase.vue
+++ src/components/MovieShowcase.vue
@@ -1,22 +1,28 @@
 <template>
   <div class="text-h2 text-lightest q-pb-md">{{ title }}</div>
-  <q-scroll-area
-    style="height: 575px;"
-    dark
-  >
-    <div class="row q-col-gutter-x-md no-wrap">
-      <movie-item v-for="(movie, i) in movies" :key="i" :movie="movie" />
-    </div>
-  </q-scroll-area>
+  <div class="row q-col-gutter-md">
+    <movie-showcase-item
+      v-for="(movie, i) in !dense ? movies : movies.slice(0, 7)"
+      :key="i"
+      :movie="movie"
+    />
+    <movie-showcase-summary-item
+      v-if="dense"
+      :movies="movies.slice(7, 16)"
+      :discover-query="discoverQuery"
+    />
+  </div>
 </template>
 
 <script>
-import MovieItem from "components/MovieShowcaseItem";
+import MovieShowcaseItem from "components/MovieShowcaseItem";
+import MovieShowcaseSummaryItem from "components/MovieShowcaseSummaryItem";
 
 export default {
   name: 'MovieShowcase',
   components: {
-    MovieItem
+    MovieShowcaseItem,
+    MovieShowcaseSummaryItem
   },
   props: {
     title: {
@@ -26,6 +32,14 @@ export default {
     movies: {
       type: Array,
       required: true
+    },
+    discoverQuery: {
+      type: String,
+      required: true
+    },
+    dense: {
+      type: Boolean,
+      default: false
     }
   }
 }
