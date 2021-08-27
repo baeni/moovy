02f2cdf6b28b5d614562fc0bc91ba9ab605ef185
baeni <baeni.saa@gmail.com>
2021-08-27 16:53:49 +0200

    Update actions.js

--- src/store/search_module/actions.js
+++ src/store/search_module/actions.js
@@ -3,42 +3,68 @@ import { useI18n } from 'vue-i18n';
 
 const TMDB_API_KEY = process.env.VUE_APP_TMDB_API_KEY;
 
-export function fetchNowPlayingMovies(context) {
-  return axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${TMDB_API_KEY}&language=${useI18n().locale}`)
-    .then(response => {
-      context.commit(`setNowPlayingMovies`, response.data.results);
-    })
-    .catch(err => Promise.reject(err));
+export function fetchPopularMovies(context) {
+  const options = {
+    method: 'GET',
+    url: 'https://api.themoviedb.org/3/movie/popular',
+    params: {
+      api_key: TMDB_API_KEY,
+      language: useI18n().locale
+    }
+  }
+
+  axios.request(options)
+    .then(res => {
+      context.commit('setPopularMovies', res.data.results);
+    }).catch(err => Promise.reject(err));
 }
 
-export function fetchTrendingMovies(context, timeWindow) {
-  return axios.get(`https://api.themoviedb.org/3/trending/movie/${timeWindow}?api_key=${TMDB_API_KEY}&language=${useI18n().locale}`)
-    .then(response => {
-      context.commit(`setTrendingMovies${timeWindow}`, response.data.results);
-    })
-    .catch(err => Promise.reject(err));
-}
+export function fetchUpcomingMovies(context) {
+  const options = {
+    method: 'GET',
+    url: 'https://api.themoviedb.org/3/movie/upcoming',
+    params: {
+      api_key: TMDB_API_KEY,
+      language: useI18n().locale
+    }
+  }
 
-export function fetchPopularActors(context) {
-  return axios.get(`https://api.themoviedb.org/3/person/popular?api_key=${TMDB_API_KEY}&language=${useI18n().locale}`)
-    .then(response => {
-      context.commit(`setPopularActors`, response.data.results);
-    })
-    .catch(err => Promise.reject(err));
+  axios.request(options)
+    .then(res => {
+      context.commit('setUpcomingMovies', res.data.results.filter(movie => new Date(movie.release_date) > new Date()));
+    }).catch(err => Promise.reject(err));
 }
 
 export function searchMovie(context, searchQuery) {
-  return axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${TMDB_API_KEY}&query=${searchQuery}&language=${useI18n().locale}`)
-    .then(response => {
-      return response.data.results;
-    })
-    .catch(err => Promise.reject(err));
+  const options = {
+    method: 'GET',
+    url: 'https://api.themoviedb.org/3/search/movie',
+    params: {
+      api_key: TMDB_API_KEY,
+      language: useI18n().locale,
+      query: searchQuery
+    }
+  }
+
+  return axios.request(options)
+    .then(res => {
+      return res.data.results;
+    }).catch(err => Promise.reject(err));
 }
 
 export function getMovieDetails(context, movieId) {
-  return axios.get(`https://api.themoviedb.org/3/movie/${movieId}?append_to_response=videos&api_key=${TMDB_API_KEY}&language=${useI18n().locale}`)
-    .then(response => {
-      return response.data;
-    })
-    .catch(err => Promise.reject(err));
+  const options = {
+    method: 'GET',
+    url: 'https://api.themoviedb.org/3/movie/'+movieId,
+    params: {
+      api_key: TMDB_API_KEY,
+      language: useI18n().locale,
+      append_to_response: 'videos'
+    }
+  }
+
+  return axios.request(options)
+    .then(res => {
+      return res.data;
+    }).catch(err => Promise.reject(err));
 }
