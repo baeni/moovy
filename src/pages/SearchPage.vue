e992f9d451540b97f6b300e26c21fdd82b9cb8a7
baeni <baeni.saa@gmail.com>
2021-08-27 17:00:03 +0200

    Add `query`-property; Remove fetch-functions; Minor adjustments

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -2,56 +2,39 @@
   <q-page>
     <section>
       <div class="container">
-        <movie-list :title="$t('searchPage.results', { searchQuery })" :movies="movies" />
+        <movie-list
+          :title="$t('searchPage.results', { query })"
+          :movies="movies"
+        />
       </div>
     </section>
   </q-page>
 </template>
 
 <script>
-import MovieList from "components/MovieList";
+import MovieList from 'components/MovieList';
 
 export default {
   name: 'SearchPage',
   components: {
     MovieList
   },
+  props: {
+    query: {
+      type: String,
+      required: true
+    }
+  },
   data() {
     return {
-      searchQuery: this.$route.params.searchQuery,
-      discoverQuery: this.$route.params.discoverQuery,
       movies: []
     }
   },
-  async mounted() {
-    if(this.searchQuery) {
-      await this.$store.dispatch('SearchModule/searchMovie', this.searchQuery)
-        .then(response => this.movies = response)
-        .catch(err => Promise.reject(err));
-    } else if(this.discoverQuery) {
-      switch(this.discoverQuery) {
-        case 'now-playing':
-          this.movies = await this.$store.getters["SearchModule/getNowPlayingMovies"];
-          break;
-
-        case 'trending-day':
-          this.movies = await this.$store.getters["SearchModule/getTrendingMoviesDay"];
-          break;
-
-        case 'trending-week':
-          this.movies = await this.$store.getters["SearchModule/getTrendingMoviesWeek"];
-          break;
-
-      }
-    }
-  },
   created() {
-    if(this.discoverQuery) {
-      this.$store.dispatch('SearchModule/fetchNowPlayingMovies');
-      this.$store.dispatch('SearchModule/fetchTrendingMovies', "Day");
-      this.$store.dispatch('SearchModule/fetchTrendingMovies', "Week");
-      this.$store.dispatch('SearchModule/fetchPopularActors');
-    }
+    this.$store.dispatch('SearchModule/searchMovie', this.query)
+      .then(res => {
+        this.movies = res
+      }).catch(err => Promise.reject(err));
   }
 }
 </script>
