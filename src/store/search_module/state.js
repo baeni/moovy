2a5b30ee50bf2baf100b834552c8f36ba01f6bbf
baeni <baeni.saa@gmail.com>
2021-08-27 16:53:18 +0200

    Update state.js

--- src/store/search_module/state.js
+++ src/store/search_module/state.js
@@ -1,8 +1,6 @@
 export default function () {
   return {
-    nowPlayingMovies: [],
-    trendingMoviesDay: [],
-    trendingMoviesWeek: [],
-    popularActors: []
+    popularMovies: [],
+    upcomingMovies: []
   }
 }
