0df39a447538c9aa0d0782c21293242d48793f73
baeni <baeni.saa@gmail.com>
2021-08-27 16:53:27 +0200

    Update mutations.js

--- src/store/search_module/mutations.js
+++ src/store/search_module/mutations.js
@@ -1,15 +1,7 @@
-export function setNowPlayingMovies(state, movies) {
-  state.nowPlayingMovies = movies;
+export function setPopularMovies(state, movies) {
+  state.popularMovies = movies;
 }
 
-export function setTrendingMoviesDay(state, movies) {
-  state.trendingMoviesDay = movies;
-}
-
-export function setTrendingMoviesWeek(state, movies) {
-  state.trendingMoviesWeek = movies;
-}
-
-export function setPopularActors(state, actors) {
-  state.popularActors = actors;
+export function setUpcomingMovies(state, movies) {
+  state.upcomingMovies = movies;
 }
