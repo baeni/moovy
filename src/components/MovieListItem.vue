146f1af55bcb7bcd5f54199fffe60774d83f8e89
baeni <baeni.saa@gmail.com>
2021-08-27 16:55:59 +0200

    Make this the ShowcaseItem- & ListItem-component

--- src/components/MovieListItem.vue
+++ src/components/MovieListItem.vue
@@ -1,23 +1,36 @@
 <template>
-  <div class="col-6">
+  <div class="col-3" v-bind:class="{ 'col-6': $q.platform.is.mobile }">
     <q-card
-      class="row full-height cursor-pointer"
+      class="full-height"
       bordered
       dark
-      @click="$router.push('/details/movie/'+movie.id)"
     >
-      <div class="col-grow overflow-hidden">
+      <div class="overflow-hidden">
         <q-img
+          class="cursor-pointer"
           :src="'https://image.tmdb.org/t/p/w500'+movie.poster_path"
-          width="150px"
-          height="100%"
-        />
+          :ratio="2/3"
+          no-native-menu
+          @click="$router.push('/details/'+movie.id)"
+        >
+          <template v-slot:error>
+            <div class="text-h4 text-center bg-transparent absolute-full flex flex-center">No image available</div>
+          </template>
+        </q-img>
       </div>
 
-      <q-card-section class="col q-my-auto">
-        <div class="text-h4">{{ movie.title }}</div>
-        <div class="text-subtitle2 text-light q-pb-md">{{ movie.release_date }}</div>
-        <div class="text-body1 ellipsis-2-lines">{{ movie.overview }}</div>
+      <q-card-section>
+        <div class="text-h4 ellipsis-2-lines">{{ movie.title }}</div>
+        <div class="text-subtitle2 text-light">{{ movie.release_date }}</div>
+        <q-chip
+          v-for="genreId in movie.genre_ids"
+          :key="genreId"
+          :label="genreId"
+          size="12px"
+          color="darkest"
+          text-color="lightest"
+          clickable
+        />
       </q-card-section>
     </q-card>
   </div>
@@ -36,14 +49,20 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+#movie-list-item {
+  @media (max-width: $breakpoint-sm) {
+    width: 95vw;
+  }
+}
+
 .q-card {
   &:hover .q-img {
-    transition: transform 1s ease-in-out;
+    transition: transform .75s ease-in-out;
     transform: scale(1.05);
   }
 }
 
 .q-img {
-  transition: transform .25s ease-in-out;
+  transition: transform .175s ease-in-out;
 }
 </style>
