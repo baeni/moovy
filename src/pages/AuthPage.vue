cb737f3914fa499e790c57cdfa7f933ccedce05b
baeni <baeni.saa@gmail.com>
2021-09-05 21:22:27 +0200

    Create AuthPage.vue

---
+++ src/pages/AuthPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page>
+    <!-- landing -->
+    <section>
+      <div class="container">
+        <auth-form />
+      </div>
+    </section>
+  </q-page>
+</template>
+
+<script>
+import AuthForm from "components/auth/AuthForm";
+
+export default {
+  name: 'AuthPage',
+  components: {
+    AuthForm
+  }
+}
+</script>
