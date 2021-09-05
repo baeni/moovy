b1bcd80b83624d3f19388a0d78ca75d97843e3d9
baeni <baeni.saa@gmail.com>
2021-09-05 21:28:17 +0200

    Create YourListPage.vue

---
+++ src/pages/YourListPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page>
+    <section>
+      <div class="container">
+        <q-btn :label="$store.dispatch('StoreModule/getList')" />
+      </div>
+    </section>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'YourListPage'
+}
+</script>
