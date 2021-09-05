bc3b7c4dd333760b75c479111680891aafddffff
baeni <baeni.saa@gmail.com>
2021-09-05 21:27:29 +0200

    Add routes for `auth`- & `your-list`-paths

--- src/router/routes.js
+++ src/router/routes.js
@@ -8,6 +8,9 @@ const routes = [
       { path: 'search/:query', component: () => import('pages/SearchPage.vue'), props: route => ({ query: route.params.query }) },
       { path: 'details/:id', component: () => import('pages/DetailsPage.vue'), props: route => ({ id: route.params.id }) },
 
+      { path: 'auth', component: () => import('pages/AuthPage') },
+      { path: 'your-list', component: () => import('pages/YourListPage.vue') },
+
       { path: ':catchAll(.*)*', component: () => import('pages/Error404Page.vue') }
     ]
   }
