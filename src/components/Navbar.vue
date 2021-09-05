de716c15797c31c49d5fad94a3b6e4b08cff23b4
baeni <baeni.saa@gmail.com>
2021-09-05 21:25:46 +0200

    Adjustments to use `UserProfile`-component; Minor changes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,74 +1,35 @@
 <template>
   <q-header class="flex">
     <q-toolbar class="container">
-      <q-btn class="text-h3" label="moovy" flat @click="$router.push('/')" no-caps />
+      <q-btn class="text-h3" label="moovy" flat no-caps @click="$router.push('/')" />
 
       <q-space />
 
-      <search-form dense />
+      <search-form class="q-ml-md" dense />
 
-      <q-separator
-        class="q-mx-md"
-        vertical
-        inset
-        dark
-      />
+      <q-separator class="q-mx-md" vertical inset dark />
 
-      <q-btn
-        class="text-bold"
-        :label="locale"
-        flat
-      >
-        <q-menu
-          :offset="[0, 10]"
-          transition-show="scale"
-          transition-hide="scale"
-          dark
-        >
-          <q-list>
-            <q-item
-              v-for="(localeOption, i) in localeOptions"
-              :key="i"
-              clickable
-              dense
-              v-close-popup
-              @click="locale = localeOption"
-            >
-              <q-item-section
-                class="text-uppercase"
-                v-bind:class="{'text-bold text-underline':locale === localeOption}"
-              >
-                {{ localeOption }}
-              </q-item-section>
-            </q-item>
-          </q-list>
-        </q-menu>
-      </q-btn>
+      <user-profile v-if="user" :user="user" />
+      <q-btn v-else class="text-bold" :label="$t('authPage.name')" flat @click="$router.push('/auth')" />
     </q-toolbar>
   </q-header>
 </template>
 
 <script>
-import { useI18n } from 'vue-i18n';
-
-import SearchForm from "components/SearchForm";
+import SearchForm from 'components/SearchForm';
+import UserProfile from 'components/UserProfile';
 
 export default {
   name: 'Navbar',
   components: {
-    SearchForm
+    SearchForm,
+    UserProfile
   },
-  setup() {
-    const { locale } = useI18n({ useScope: 'global' });
-
-    return {
-      locale,
-      localeOptions: [
-        "en",
-        "de"
-      ]
+  computed: {
+    user() {
+      return this.$store.getters['AuthModule/getUser']
     }
-  }
+  },
 }
 </script>
 
