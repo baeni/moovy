2b96ffdc1fc43a8b5c956c85527869336f9b252b
baeni <baeni.saa@gmail.com>
2021-09-10 11:38:19 +0200

    Add `onlyNonAuth` & `onlyAuth` meta-data to specific paths

--- src/router/routes.js
+++ src/router/routes.js
@@ -8,8 +8,8 @@ const routes = [
       { path: 'search/:query', component: () => import('pages/SearchPage.vue'), props: route => ({ query: route.params.query }) },
       { path: 'details/:id', component: () => import('pages/DetailsPage.vue'), props: route => ({ id: route.params.id }) },
 
-      { path: 'auth', component: () => import('pages/AuthPage') },
-      { path: 'my-list', component: () => import('pages/MyListPage.vue') },
+      { path: 'auth', component: () => import('pages/AuthPage'), meta: { onlyNonAuth: true }  },
+      { path: 'my-list', component: () => import('pages/MyListPage.vue'), meta: { onlyAuth: true } },
 
       { path: ':catchAll(.*)*', component: () => import('pages/Error404Page.vue') }
     ]
