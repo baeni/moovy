036ca953d53ecb28bd9f811a4e52fd00f7fa3712
baeni <baeni.saa@gmail.com>
2021-09-10 11:34:02 +0200

    Add `subscribeUsersMyList`-function

--- src/store/db_module/actions.js
+++ src/store/db_module/actions.js
@@ -1,5 +1,11 @@
-import { doc, getDoc, setDoc, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore";
+import { doc, getDoc, setDoc, updateDoc, arrayUnion, arrayRemove, onSnapshot } from "firebase/firestore";
 import db from 'src/boot/firebase';
+
+export async function subscribeUsersMyList(context) {
+  await onSnapshot(doc(db, 'users', context.rootGetters["AuthModule/getUser"].email), doc => {
+    context.commit('setUsersMyList', doc.data().myList);
+  });
+}
 
 export async function addToUsersMyList(context, movie) {
   const docRef = doc(db, 'users', context.rootGetters["AuthModule/getUser"].email);
