91404a9b291e9808986568e47bf6442f188bc8a8
baeni <baeni.saa@gmail.com>
2021-09-13 17:44:41 +0200

    Create AuthList.vue

---
+++ src/components/auth/AuthList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="text-center q-gutter-x-sm">
+    <!-- Google -->
+    <q-btn
+      class="col text-bold text-lightest bg-light"
+      label="Google"
+      icon="fab fa-google"
+      flat
+      no-caps
+      rounded
+      @click="this.$store.dispatch('AuthModule/signInGoogle')"
+    />
+
+    <!-- GitHub -->
+    <q-btn
+      class="col text-bold text-lightest bg-light"
+      label="Github"
+      icon="fab fa-github"
+      flat
+      no-caps
+      rounded
+      @click="this.$store.dispatch('AuthModule/signInGithub')"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AuthList'
+}
+</script>
