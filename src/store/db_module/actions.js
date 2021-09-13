611d32b9cef05220a9a6af9575c3bb317edf00d7
baeni <baeni.saa@gmail.com>
2021-09-13 14:19:16 +0200

    Replace "MyList"-functions with "Watchlist"-functions; Add functions for managing "Favorites"

--- src/store/db_module/actions.js
+++ src/store/db_module/actions.js
@@ -1,33 +1,64 @@
 import { doc, getDoc, setDoc, updateDoc, arrayUnion, onSnapshot } from "firebase/firestore";
 import db from 'src/boot/firebase';
 
-export async function subscribeUsersMyList(context) {
+export async function subscribeUsersWatchlist(context) {
   const docRef = doc(db, 'users', context.rootGetters["AuthModule/getUser"].email);
 
   await onSnapshot(docRef, doc => {
-    context.commit('setUsersMyList', doc.data().myList);
+    context.commit('setUsersWatchlist', doc.data().watchlist);
   });
 }
 
-export async function addToUsersMyList(context, movie) {
+export async function addToUsersWatchlist(context, movie) {
   const docRef = doc(db, 'users', context.rootGetters["AuthModule/getUser"].email);
   const docSnap = await getDoc(docRef);
 
   if (!docSnap.exists()) {
     await setDoc(docRef, {
-      myList: arrayUnion(movie)
+      watchlist: arrayUnion(movie)
     });
   } else {
     await updateDoc(docRef, {
-      myList: arrayUnion(movie)
+      watchlist: arrayUnion(movie)
     });
   }
 }
 
-export async function removeFromUsersMyList(context, movie) {
+export async function removeFromUsersWatchlist(context, movie) {
   const docRef = doc(db, 'users', context.rootGetters["AuthModule/getUser"].email);
 
   await updateDoc(docRef, {
-    myList: context.getters.getUsersMyList.filter(el => el.id !== movie.id)
+    watchlist: context.getters.getUsersWatchlist.filter(el => el.id !== movie.id)
+  });
+}
+
+export async function subscribeUsersFavorites(context) {
+  const docRef = doc(db, 'users', context.rootGetters["AuthModule/getUser"].email);
+
+  await onSnapshot(docRef, doc => {
+    context.commit('setUsersFavorites', doc.data().favorites);
+  });
+}
+
+export async function addToUsersFavorites(context, movie) {
+  const docRef = doc(db, 'users', context.rootGetters["AuthModule/getUser"].email);
+  const docSnap = await getDoc(docRef);
+
+  if (!docSnap.exists()) {
+    await setDoc(docRef, {
+      favorites: arrayUnion(movie)
+    });
+  } else {
+    await updateDoc(docRef, {
+      favorites: arrayUnion(movie)
+    });
+  }
+}
+
+export async function removeFromUsersFavorites(context, movie) {
+  const docRef = doc(db, 'users', context.rootGetters["AuthModule/getUser"].email);
+
+  await updateDoc(docRef, {
+    favorites: context.getters.getUsersFavorites.filter(el => el.id !== movie.id)
   });
 }
