f247265e56f5bd71a73f9cf14e4ef15e4969e749
baeni <baeni.saa@gmail.com>
2021-09-13 14:16:50 +0200

    Increase gutter; Redesign details; Add information; Implement "Watchlist" & "Favorites"

--- src/pages/DetailsPage.vue
+++ src/pages/DetailsPage.vue
@@ -7,7 +7,7 @@
         style="z-index: -99; filter: blur(15px) opacity(.25);"
         :src="'https://image.tmdb.org/t/p/w1280'+movie.backdrop_path"
       />
-      <div class="container row q-col-gutter-x-lg">
+      <div class="container row q-col-gutter-x-md">
         <q-img
           class="col-4 rounded-borders"
           :src="'https://www.themoviedb.org/t/p/w500/'+movie.poster_path"
@@ -20,11 +20,34 @@
         </q-img>
 
         <div class="col-8 q-my-auto">
-          <div class="text-h2 text-white">{{ movie.title }}</div>
-          <div class="text-subtitle2 text-lightest">{{ movie.release_date }} • {{ movie.runtime }}min</div>
+          <movie-genre-chips class="q-pb-sm" :movie="movie" />
+          <div class="text-h2 text-white q-pb-sm">{{ movie.title }}</div>
+          <div class="text-body1 text-white q-pb-lg">{{ movie.overview }}</div>
 
-          <div class="text-body1 text-white q-py-md">{{ movie.overview }}</div>
+          <!-- Information -->
+          <div class="row q-pb-lg">
+            <div class="col">
+              <div class="text-subtitle1 text-lightest">Release Date</div>
+              <div class="text-h3 text-white">{{ releaseDate }}</div>
+            </div>
 
+            <div class="col">
+              <div class="text-subtitle1 text-lightest">Runtime</div>
+              <div class="text-h3 text-white">{{ runtime }}</div>
+            </div>
+
+            <div class="col">
+              <div class="text-subtitle1 text-lightest">TMDb Rating</div>
+              <div class="text-h3 text-white">{{ tmdbRating }}</div>
+            </div>
+
+            <div class="col">
+              <div class="text-subtitle1 text-lightest">TMDb Votes</div>
+              <div class="text-h3 text-white">{{ tmdbVotes }}</div>
+            </div>
+          </div>
+
+          <!-- Homepage -->
           <q-btn
             class="text-bold text-white bg-light q-mr-sm"
             :label="$t('detailsPage.homepage')"
@@ -36,43 +59,85 @@
             no-caps
           />
 
+          <!-- Watchlist -->
           <q-btn
-            v-show="!existsInUsersMyList"
-            class="text-bold text-accent"
+            v-show="!existsInUsersWatchlist"
+            class="text-white"
             icon="bookmark_border"
             dark
             flat
             round
             dense
             no-caps
-            @click="addToUsersMyList()"
+            @click="addToUsersWatchlist()"
           >
             <q-tooltip
               class="bg-light text-caption text-bold"
               transition-show="scale"
               transition-hide="fade"
             >
-              Add to My List
+              {{ $t('detailsPage.addToUsersWatchlist') }}
             </q-tooltip>
           </q-btn>
 
           <q-btn
-            v-show="existsInUsersMyList"
-            class="text-bold text-accent"
+            v-show="existsInUsersWatchlist"
+            class="text-accent"
             icon="bookmark"
             dark
             flat
             round
             dense
             no-caps
-            @click="removeFromUsersMyList()"
+            @click="removeFromUsersWatchlist()"
           >
             <q-tooltip
               class="bg-light text-caption text-bold"
               transition-show="scale"
               transition-hide="fade"
             >
-              Remove from My List
+              {{ $t('detailsPage.addToUsersWatchlist') }}
+            </q-tooltip>
+          </q-btn>
+
+          <!-- Favorites -->
+          <q-btn
+            v-show="!existsInUsersFavorites"
+            class="text-white"
+            icon="favorite_border"
+            dark
+            flat
+            round
+            dense
+            no-caps
+            @click="addToUsersFavorites()"
+          >
+            <q-tooltip
+              class="bg-light text-caption text-bold"
+              transition-show="scale"
+              transition-hide="fade"
+            >
+              {{ $t('detailsPage.addToUsersFavorites') }}
+            </q-tooltip>
+          </q-btn>
+
+          <q-btn
+            v-show="existsInUsersFavorites"
+            class="text-red"
+            icon="favorite"
+            dark
+            flat
+            round
+            dense
+            no-caps
+            @click="removeFromUsersFavorites()"
+          >
+            <q-tooltip
+              class="bg-light text-caption text-bold"
+              transition-show="scale"
+              transition-hide="fade"
+            >
+              {{ $t('detailsPage.removeFromUsersFavorites') }}
             </q-tooltip>
           </q-btn>
         </div>
@@ -154,9 +219,13 @@
 
 <script>
 import { useQuasar } from 'quasar'
+import MovieGenreChips from 'components/movie/MovieGenreChips';
 
 export default {
   name: 'DetailsPage',
+  components: {
+    MovieGenreChips
+  },
   props: {
     id: {
       type: String,
@@ -171,35 +240,78 @@ export default {
     }
   },
   methods: {
-    addToUsersMyList() {
+    addToUsersWatchlist() {
       if (this.$store.getters["AuthModule/getUser"] === null) {
         this.$router.push('/auth');
         return;
       }
 
-      this.$store.dispatch('DbModule/addToUsersMyList', this.movie)
+      this.$store.dispatch('DbModule/addToUsersWatchlist', this.movie)
         .then(() => {
-          this.notify(this.$t('detailsPage.addedToUsersList', { title: this.movie.title }));
+          this.notify(this.$t('detailsPage.addedToUsersWatchlist', { title: this.movie.title }));
+        }).catch(err => Promise.reject(err));
+    },
+    removeFromUsersWatchlist() {
+      if (this.$store.getters["AuthModule/getUser"] === null) {
+        this.$router.push('/auth');
+        return;
+      }
+
+      this.$store.dispatch('DbModule/removeFromUsersWatchlist', this.movie)
+        .then(() => {
+          this.notify(this.$t('detailsPage.removedFromUsersWatchlist', { title: this.movie.title }));
         }).catch(err => Promise.reject(err));
     },
 
-    removeFromUsersMyList() {
+    addToUsersFavorites() {
       if (this.$store.getters["AuthModule/getUser"] === null) {
         this.$router.push('/auth');
         return;
       }
 
-      this.$store.dispatch('DbModule/removeFromUsersMyList', this.movie)
+      this.$store.dispatch('DbModule/addToUsersFavorites', this.movie)
         .then(() => {
-          this.notify(this.$t('detailsPage.removedFromUsersList', { title: this.movie.title }));
+          this.notify(this.$t('detailsPage.addedToUsersFavorites', { title: this.movie.title }));
+        }).catch(err => Promise.reject(err));
+    },
+    removeFromUsersFavorites() {
+      if (this.$store.getters["AuthModule/getUser"] === null) {
+        this.$router.push('/auth');
+        return;
+      }
+
+      this.$store.dispatch('DbModule/removeFromUsersFavorites', this.movie)
+        .then(() => {
+          this.notify(this.$t('detailsPage.removedFromUsersFavorites', { title: this.movie.title }));
         }).catch(err => Promise.reject(err));
     }
   },
   computed: {
-    existsInUsersMyList() {
+    releaseDate() {
+      const date = new Date(this.movie.release_date);
+      return date.getFullYear();
+    },
+    runtime() {
+      const runtime = this.movie.runtime;
+      return runtime ? runtime+' min' : this.$t('detailsPage.noInformation');
+    },
+    tmdbRating() {
+      const rating = this.movie.vote_average;
+      return rating ? rating+' /10' : this.$t('detailsPage.noInformation');
+    },
+    tmdbVotes() {
+      const rating = this.movie.vote_count;
+      return rating ? rating : this.$t('detailsPage.noInformation');
+    },
+    existsInUsersWatchlist() {
       return this.$store.getters['AuthModule/getUser'] !== null
-        && this.$store.getters["DbModule/getUsersMyList"] !== undefined
-        && this.$store.getters["DbModule/getUsersMyList"].some(el => el.title === this.movie.title);
+        && this.$store.getters["DbModule/getUsersWatchlist"] !== undefined
+        && this.$store.getters["DbModule/getUsersWatchlist"].some(el => el.title === this.movie.title);
+    },
+    existsInUsersFavorites() {
+      return this.$store.getters['AuthModule/getUser'] !== null
+        && this.$store.getters["DbModule/getUsersFavorites"] !== undefined
+        && this.$store.getters["DbModule/getUsersFavorites"].some(el => el.title === this.movie.title);
     }
   },
   setup() {
