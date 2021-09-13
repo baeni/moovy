7a091fe7d4302c503f06c89494bf68233f13b780
baeni <baeni.saa@gmail.com>
2021-09-13 14:13:39 +0200

    Update EN

--- src/i18n/en/index.js
+++ src/i18n/en/index.js
@@ -4,42 +4,52 @@ export default {
     formSearchHint: 'moovy is based on the TMDb and fetches any and all information from their API',
 
     profileMenu: 'Signed in as {username}',
-    profileMenuAccount: 'My Account',
-    profileMenuList: 'My List',
-    profileMenuLanguage: 'Language',
+    profileMenuWatchlist: 'My Watchlist',
+    profileMenuFavorites: 'My Favorites',
     profileMenuSignOut: 'Sign out'
   },
   error: {
     signInNecessary: 'You need to be signed!'
   },
   indexPage: {
-    name: 'Home',
     heading: 'Find the movie with moovy',
     subHeading: 'Pronounced as /mu:vɪ/ or movie',
+
     popularMovies: 'Popular Movies',
-    upcomingMovies: 'Upcoming Movies',
-    more: 'Show more'
+    upcomingMovies: 'Upcoming Movies'
   },
   searchPage: {
     results: 'Results for "{query}"'
   },
   detailsPage: {
+    noInformation: 'No information',
     homepage: 'Official Site',
-    trailers: 'Trailers, Teasers & more',
-    addedToUsersList: '"{title}" has been added to your list.',
-    removedFromUsersList: '"{title}" has been removed from your list.'
+
+    addToUsersWatchlist: 'Add to watchlist',
+    removeFromUsersWatchlist: 'Remove from watchlist',
+    addedToUsersWatchlist: '"{title}" has been added to your watchlist.',
+    removedFromUsersWatchlist: '"{title}" has been removed from your watchlist.',
+
+    addToUsersFavorites: 'Add to favorites',
+    removeFromUsersFavorites: 'Remove from favorites',
+    addedToUsersFavorites: '"{title}" has been added to your favorites.',
+    removedFromUsersFavorites: '"{title}" has been removed from your favorites.'
   },
   authPage: {
     name: 'Sign In',
     heading: 'Sign in with',
+
     formEmail: 'Enter your email',
     formPassword: 'Enter your password',
     formPasswordConfirm: 'Confirm your password',
     formSignIn: 'Sign In',
     formSignUp: 'Create New Account'
   },
-  myListPage: {
-    name: 'My List'
+  watchlistPage: {
+    name: 'My Watchlist'
+  },
+  favoritesPage: {
+    name: 'My Favorites'
   },
   error404Page: {
     heading: '404 — Wrong direction',
