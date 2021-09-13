18b8ed1b5ae8715602366cfe8d3e295f12134ad7
baeni <baeni.saa@gmail.com>
2021-09-13 14:10:30 +0200

    Create MovieGenreChips.vue

---
+++ src/components/movie/MovieGenreChips.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-chip
+    v-for="genre in movie.genres"
+    :key="genre.id"
+    :label="genre.name"
+    size="12px"
+    color="lightest"
+    text-color="black"
+    square
+  />
+</template>
+
+<script>
+export default {
+  name: 'MovieGenreChips',
+  props: {
+    movie: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
