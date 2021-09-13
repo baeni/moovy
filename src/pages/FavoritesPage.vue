c19e23035ff3ac87467cd7971412f7d7430dd088
baeni <baeni.saa@gmail.com>
2021-09-13 14:05:19 +0200

    Create FavoritesPage.vue

---
+++ src/pages/FavoritesPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-page>
+    <section>
+      <div class="container">
+        <movie-list
+          :title="$t('favoritesPage.name')"
+          :movies="usersFavorites"
+        />
+      </div>
+    </section>
+  </q-page>
+</template>
+
+<script>
+import MovieList from 'components/movie/MovieList';
+
+export default {
+  name: 'FavoritesPage',
+  components: {
+    MovieList
+  },
+  computed: {
+    usersFavorites() {
+      return this.$store.getters['DbModule/getUsersFavorites']
+    }
+  }
+}
+</script>
