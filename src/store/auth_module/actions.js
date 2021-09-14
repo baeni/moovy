e9892e7a8419456eb6ab27991926b51b7e3e9d71
baeni <baeni.saa@gmail.com>
2021-09-14 12:12:24 +0200

    Remove `signInGithub`-function

--- src/store/auth_module/actions.js
+++ src/store/auth_module/actions.js
@@ -1,19 +1,7 @@
-import { GoogleAuthProvider, GithubAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 
 export function signInGoogle(context) {
   const provider = new GoogleAuthProvider();
-
-  signInWithPopup(getAuth(), provider)
-    .then(() => {
-      context.commit('setUser');
-      this.dispatch('DbModule/subscribeUsersWatchlist');
-      this.dispatch('DbModule/subscribeUsersFavorites');
-      this.$router.back();
-    }).catch(err => Promise.reject(err));
-}
-
-export function signInGithub(context) {
-  const provider = new GithubAuthProvider();
 
   signInWithPopup(getAuth(), provider)
     .then(() => {
